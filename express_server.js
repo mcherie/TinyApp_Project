dd79c8e8888e4138ff08a3fe5b09588171a8c31b
Cher <red_phoenix417@yahoo.com>
2019-06-04 23:55:26 +0000

    created  a form for user to imput and then a POST request. Then created a route that would render this form. Then created a route to handle the POST request. Then used body-parser library. Then generated a random string generator to serve as shortURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -9,7 +12,9 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
+function generateRandomString() {
+    return  Math.floor((1 + Math.random()) * 0x10000).toString(36).substring(1);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,6 +35,16 @@ app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
 
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
