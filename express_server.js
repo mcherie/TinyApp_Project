52fa9d7470bbf5aa792176b9558886f1b210741f
Cher <red_phoenix417@yahoo.com>
2019-06-05 04:29:52 +0000

    adding link to the tinyURL, reformatting code to comment in case need to troubleshoot by adding http

--- express_server.js
+++ express_server.js
@@ -1,61 +1,77 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+var express = require('express')
+var app = express()
+var PORT = 8080 // default port 8080
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-function generateRandomString() {
-    return  Math.floor((1 + Math.random()) * 0x10000000).toString(36);
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function generateRandomString () {
+  return Math.floor((1 + Math.random()) * 0x10000000).toString(36)
+}
 
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+})
 
-  
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n')
+})
 
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars) // redirect //
+})
 
-app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-});
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+app.post('/urls', (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  let newShortURL = generateRandomString() // to generate a new random string
+  // console.log(newShortURL)
 
-app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
+    // const formattedURL = function(incomingLongURL) {
+    //     let something 
+    //     if (incomingLongURL.startsWith("http")) {
+    //         return incomingLongURL; 
+    //     } else {
+    //         return "http://" + incomingLongURL
+    //     }
+    // } 
+    // let newLongURL = formattedURL(incomingLongURL)
 
+// I can declare a new variable here to use, then replace the req.body.longURL later with incomingLongURL
+    // const incomingLongURL = req.body.longURL 
+  urlDatabase[newShortURL] = req.body.longURL // to add this new info into the urlDatabase
+  console.log(urlDatabase) // just for me to see if it got added to the database
+  res.redirect('/urls/' + newShortURL) // to redirect to the page which shows his newly created tiny URL
+})
 
-app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
-});
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = req.body.longURL
+  res.redirect(longURL)
+})
 
-  
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  }
+  res.render('urls_show', templateVars)
+})
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-  console.log(generateRandomString());
-});
-
-
+  console.log(`Example app listening on port ${PORT}!`)
+})
