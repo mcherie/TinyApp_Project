23840451cddfca4bf7deb44e6be26f28a43e841e
Cher <red_phoenix417@yahoo.com>
2019-06-09 17:44:32 +0000

    Change cookie-parser to cookie-session and update code to use session

--- express_server.js
+++ express_server.js
@@ -4,8 +4,17 @@ const bcrypt = require('bcrypt');
 var app = express()
 var PORT = 8080 // default port 8080
 
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+// const cookieParser = require('cookie-parser')
+// app.use(cookieParser())
+
+const cookieSession = require('cookie-session')
+app.use(cookieSession({
+    name: 'session',
+    keys: ['lighthouse-labs'],
+  
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }))
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -84,7 +93,7 @@ function urlsForUser(id) {
 
 
 app.get('/urls', (req, res) => {
-    let user_id = req.cookies.user_id;
+    let user_id = req.session.user_id;
 
     // console.log("***************", user_id)
 
@@ -102,7 +111,7 @@ app.get('/urls', (req, res) => {
 })
 
 app.get('/urls/new', (req, res) => {
-    let user_id = req.cookies.user_id;
+    let user_id = req.session.user_id;
     let user = users[user_id]
 
     if (!user) { // if they are not logged in, they can not continue
@@ -138,7 +147,7 @@ console.log("_____________________POST('urls'___________________________________
 // I can declare a new variable here to use, then replace the req.body.longURL later with incomingLongURL
     // const incomingLongURL = req.body.longURL 
     var newShortURL = generateRandomString();
-    const userID = req.cookies.user_id;
+    const userID = req.session.user_id;
     // urlDatabase[newShortURL] = {} // to add this new info into the urlDatabase PUT THIS BACK!!!
 
 // Creation of the new object url id
@@ -149,7 +158,9 @@ console.log("_____________________POST('urls'___________________________________
     userID
 };
 // Insertion of new person to the object
-res.cookie("user_id", userID)
+// res.cookie("user_id", userID)
+req.session.user_id = userID;
+
 // username: req.cookies.username
 console.log(urlDatabase) // just for me to see if it got added to the database
   res.redirect('/urls/' + newShortURL) // to redirect to the page which shows his newly created tiny URL
@@ -166,13 +177,16 @@ app.get('/u/:shortURL', (req, res) => {
 })
 
 
+
+
 app.get('/urls/:shortURL', (req, res) => {
-    let user_id = req.cookies.user_id;
+    let user_id = req.session.user_id;
+
     let user = users[user_id];
     let shortURL = req.params.shortURL;
     let urlObject = urlDatabase[shortURL];
     let longURL = urlObject.longURL;
-    let urlUserId = urlObject.userId;
+    let urlUserId = urlObject.userID;
 
     let templateVars = {
         shortURL: shortURL,
@@ -185,7 +199,7 @@ app.get('/urls/:shortURL', (req, res) => {
 })
 
 app.post('/urls/:shortURL/delete', (req, res) => {
-    const userId = req.cookies.user_id;
+    const userId = req.session.user_id;
 
     if (userId) { // if the are logged in
         // Get short url from DB
@@ -218,9 +232,9 @@ app.post('/urls/:shortURL/update', (req, res) => {
     // console.log("req part", req.cookies);
     console.log("_____________________POST('/update'_____________________________________")
     
-    const userID = req.cookies.user_id;
-    res.cookie("user_id", userID)
-
+    const userID = req.session.user_id;
+    // res.cookie("user_id", userID)
+    req.session.user_id = userID
 
 
     urlDatabase[shortURL] = {
@@ -229,7 +243,7 @@ app.post('/urls/:shortURL/update', (req, res) => {
     };
     // Insertion of new person to the object    
 
-    if (req.cookies.user_id) { // if they are logged in, they can continue
+    if (req.session.user_id) { // if they are logged in, they can continue
         res.redirect('/urls/' + shortURL);
     } else { // if they are not logged in go back to login
         res.redirect('/login')
@@ -252,7 +266,8 @@ app.post('/login', (req, res) => {
         const hashedPassword = user["password"];
 
         if (bcrypt.compareSync(password, hashedPassword)) {
-            res.cookie("user_id", user.id)
+            // res.cookie("user_id", user.id)
+            req.session.user_id = user.id;
             res.redirect('/urls/') // to redirect to the page which shows his newly created tiny URL
         } else {
             res.status(403);
@@ -264,7 +279,8 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
     // res.clearCookie("username");
-    res.clearCookie("user_id");
+    // res.clearCookie("user_id");
+    res.session = null;
     user = null;
     res.redirect('/login');
 })
@@ -316,8 +332,10 @@ app.post('/register', (req, res) => {
 
         // Insertion of new person to the object
         users[newUserRandomID] = eachUser;
-        res.cookie("user_id", newUserRandomID)
-        console.log(res.cookie)
+        // res.cookie("user_id", newUserRandomID)
+        // console.log(res.cookie)
+
+        req.session.user_id = newUserRandomID;
 
         console.log(users);
         // username: req.cookies.username
